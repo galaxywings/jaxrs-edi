821760cf83b0318bcb98a45549c6cd4258420af9
Ace <ace.jl.han@gmail.com>
2016-11-18 02:29:18 +0800

    avoid user password to be visible

--- dashboard/api/v1/auth/serializers.py
+++ dashboard/api/v1/auth/serializers.py
@@ -1,6 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
+        exclude = ('password', )
+
+class UserGroupSerializer(serializers.ModelSerializer):
+    groups = SerializerMethodField()
+    
+    class Meta:
+        model = get_user_model()
+        exclude = ('password', )
+        
+    def get_groups(self, obj):
+        # empty list for the time being
+        return []
+
