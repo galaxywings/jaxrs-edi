6ac8c9d38ecb495a18bce70f09ab88b3090c34f7
Ace <ace.jl.han@gmail.com>
2016-11-30 03:20:13 +0800

    add customer v1 backend api

---
+++ dashboard/api/v1/customer/views.py
@@ -0,0 +1,26 @@
+
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from api.v1.customer.serializers import CustomerSerializer
+from authx.permissions import IsAdminUser
+from customer.models import Customer
+
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = (IsAdminUser, )
+    
+    @list_route(
+        methods=['get'],
+        url_path='validate-code'
+    )
+    def validate_code(self, request):
+        qs = self.get_queryset().filter(code=request.query_params.get('q'))
+        if qs.exists():
+            result = {'detail': 'Duplicated code found'}
+        else:
+            result = {'detail': 'OK'}
+        return Response(result)
--- dashboard/api/v1/urls.py
+++ dashboard/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from .auth import urls as auth_urls
+from .customer import urls as customer_urls
 
 urlpatterns = [
     url(r'^auth/', include(auth_urls, namespace='auth')),
+    url(r'^customer/', include(customer_urls, namespace='customer')),
 ]
