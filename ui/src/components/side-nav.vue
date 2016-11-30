406364035b56e5b0823e0987d1159c61779d20d0
Ace <ace.jl.han@gmail.com>
2016-12-01 03:07:46 +0800

    refactor side-nav

--- ui/src/components/side-nav.vue
+++ ui/src/components/side-nav.vue
@@ -1,28 +1,23 @@
 <template>
   <el-menu default-active="1"
     :router="true">
-    <el-submenu index="1">
-      <template slot="title"><i class="el-icon-message"></i>导航1</template>
-      <el-menu-item-group title="分组一">
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-    </el-submenu>
     <el-menu-item index="/auth">
-      <i class="el-icon-menu"></i>Auth
+      <i class="el-icon-setting"></i>Auth
     </el-menu-item>
-    <el-menu-item index="/customer">
-      <i class="el-icon-setting"></i>客户管理
-    </el-menu-item>
+    <el-submenu index="/customer">
+      <template slot="title"><i class="el-icon-setting"></i>客户管理</template>
+      <el-menu-item index="/customer/costomers/new">新建客户</el-menu-item>
+      <el-menu-item index="/customer/customers">客户列表</el-menu-item>
+    </el-submenu>
     <el-submenu index="/task">
-      <template slot="title">
-        任务管理
-      </template>
-      <el-menu-item index="/task/services">Service</el-menu-item>
-      <el-menu-item index="/task/processes">Process</el-menu-item>
+      <template slot="title"><i class="el-icon-setting"></i>任务管理</template>
+      <el-menu-item-group title="Service">
+        <el-menu-item index="/task/services">Service列表</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Process">
+        <el-menu-item index="/task/processes/new">新建Process</el-menu-item>
+        <el-menu-item index="/task/processes">Process列表</el-menu-item>
+      </el-menu-item-group>
     </el-submenu>
     <el-menu-item index="/misc">
       <i class="el-icon-setting"></i>杂项
