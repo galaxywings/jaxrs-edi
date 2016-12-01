9ba637e88521f7848cb71d6878e594330f7588ff
Jindi Wu <kymair@gmail.com>
2016-12-01 22:45:57 +0800

    Added pyftpdlib script for ftp testing

---
+++ ftp/server.py
@@ -0,0 +1,13 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("bob", "123456", "./bob", perm="elradfmw")
+authorizer.add_user("alice", "123456", "./alice", perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 21), handler)
+server.serve_forever()
