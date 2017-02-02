4ed9e4937cd7300d63c1885375389827d54d22d5
Ace <ace.jl.han@gmail.com>
2017-02-03 02:37:37 +0800

    add `*-detail` to ui step api to ease the pain on ajax call overhead

--- dashboard/api/v1/task/views.py
+++ dashboard/api/v1/task/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework.decorators import list_route
+from rest_framework.decorators import list_route, detail_route
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_bulk.generics import BulkModelViewSet
 
@@ -27,13 +27,21 @@ class StepViewSet(BulkModelViewSet):
     #permission_classes = (IsAdminUser, )
     
     @list_route(
-        methods=('get', )
+        methods=('get', ),
+        url_path='detail'
     )
-    def full(self, request, *args, **kwargs):
+    def list_detail(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
     
+    @detail_route(
+        methods=('get', ),
+        url_path='detail'
+    )
+    def object_detail(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
     def get_serializer_class(self):
-        if self.action == 'full':
+        if self.action in ('list_detail', 'object_detail', ):
             return StepServiceSerializer
         return super().get_serializer_class()
         
