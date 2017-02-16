b0782e4a872bfd09293c49297598c501d995ad08
Jindi Wu <kymair@gmail.com>
2017-02-16 23:46:56 +0800

    IFTMBF mapping draft. Focus mapping requires further clarification.

--- smooks-edi/iftmbf/src/test/java/com/wjelogistics/edi/mapping/EdiToJavabeanTest.java
+++ smooks-edi/iftmbf/src/test/java/com/wjelogistics/edi/mapping/EdiToJavabeanTest.java
@@ -3,24 +3,33 @@ package com.wjelogistics.edi.mapping;
 
 import org.junit.Test;
 import org.milyn.Smooks;
-import org.milyn.io.StreamUtils;
 import org.milyn.payload.StringResult;
 import org.milyn.smooks.edi.EDIReaderConfigurator;
 
 import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class EdiToJavabeanTest {
 
-     @Test
+    @Test
     public void testMapping() throws Exception {
         Smooks smooks = new Smooks();
         smooks.setReaderConfig(new EDIReaderConfigurator("src/main/resources/edi-model.xml"));
         StringResult result = new StringResult();
-        smooks.filterSource(new StreamSource(new ByteArrayInputStream(
-                StreamUtils.readStream(new FileInputStream("samples/IFTMBF.091049545_MADISON MAERSK706W.txt.B721400358.txt")))), result);
-        System.out.println(result.getResult());
+
+        Files.list(Paths.get("samples"))
+                .forEach(file -> {
+                    try {
+                        System.out.println(file.getFileName());
+                        smooks.filterSource(new StreamSource(new FileReader("samples/" + file.getFileName())), result);
+                        System.out.println(result.getResult());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 
 }
