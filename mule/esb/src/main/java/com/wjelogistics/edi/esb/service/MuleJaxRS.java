cdcf145279bc0c3807f0b62c0a690a0684cb8738
rocky <wangwy@wje-logistics.com>
2017-03-01 17:13:26 +0800

    rest

--- mule/esb/src/main/java/com/wjelogistics/edi/esb/service/MuleJaxRS.java
+++ mule/esb/src/main/java/com/wjelogistics/edi/esb/service/MuleJaxRS.java
@@ -1,20 +1,26 @@
 package com.wjelogistics.edi.esb.service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
 /**
  * Created by wangwy on 2017/2/22.
  */
+@Path("/helloworld")
 public class MuleJaxRS {
 
     @GET
     @Produces("text/plain")
-    @Path("/sayHelloWithUri/{name}")
+    @Path("/{name}")
     public String sayHelloWithUri(@PathParam("name") String name)
     {
         return "Hello " + name;
+    }
+
+    @POST
+    @Produces("text/json")
+    @Path("/{body}")
+    public  String handlePost(@PathParam("body") String body){
+        System.out.println(body);
+        return body;
     }
 }
