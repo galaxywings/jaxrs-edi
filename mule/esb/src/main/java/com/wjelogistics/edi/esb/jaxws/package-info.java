fb48fb1b1209efadb894e553d9eb9cc0b736aac1
rocky <wangwy@wje-logistics.com>
2017-03-09 17:35:27 +0800

    jaxws

---
+++ mule/esb/src/main/java/com/wjelogistics/edi/esb/jaxws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://eportws.nbport.com/")
+package com.wjelogistics.edi.esb.jaxws;
---
+++ mule/esb/src/main/java/com/wjelogistics/edi/esb/service/MuleJaxWS.java
@@ -0,0 +1,7 @@
+package com.wjelogistics.edi.esb.service;
+
+/**
+ * Created by wangwy on 2017/3/3.
+ */
+public class MuleJaxWS {
+}
